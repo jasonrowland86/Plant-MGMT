71b479943bb77b2d38a29cc436ee98a2278ce26d
Jason Rowland <jasonrowland86>
2017-09-15 22:19:12 -0400

    Edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
+  resources :users, only: [:index, :new, :create, :show]
+
+  resource :session, only: [:new, :create, :destroy]
+
+  resources :plants
 end
