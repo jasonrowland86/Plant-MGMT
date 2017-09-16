8468e95a0018fe49a5d288fb1d085c7535d722a3
Jason Rowland <jasonrowland86>
2017-09-15 22:49:23 -0400

    Created sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    username = user_params[:username]
+    password = user_params[:password]
+
+    user = User.find_by_credentials(username, password)
+
+    if user
+      sign_in(user)
+      flash[:notice] = 'you signed in'
+      redirect_to users_path
+    else
+      flash[:error] = 'username or password incorrect'
+      @user = User.new(username: username)
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:notice] = 'i hate to see you go but i love watching you walk away ;)'
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
