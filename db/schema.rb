a5b9f584fbaf13f80b122b21569ea735a8b53136
Jason Rowland <jasonrowland86>
2017-09-17 15:37:07 -0400

    table for plants added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916015230) do
+ActiveRecord::Schema.define(version: 20170916025040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plants", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "size"
+    t.string "condition"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_plants_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
