c3eca3fdee68bf8bbe9a6495c9a3e08c59b2ded7
Jason Rowland <jasonrowland86>
2017-09-17 15:32:25 -0400

    Added bootstrap js and css to assets.rb to be precompiled

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
+  root 'sessions#index'
   resources :users, only: [:index, :new, :create, :show]
 
   resource :session, only: [:new, :create, :destroy]
 
   resources :plants
+
 end
