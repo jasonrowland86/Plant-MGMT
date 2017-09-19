b87e5f69e7fb20aaaebafe59b799867b18b3eb64
Jason Rowland <jasonrowland86>
2017-09-19 13:20:16 -0400

    Added routes for some plant views and events

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+  resources :events
   root 'sessions#index'
   resources :users, only: [:index, :new, :create, :show]
 
   resource :session, only: [:new, :create, :destroy]
 
+  get '/plants/dashboard' => 'plants#dashboard'
   get '/plants/wet' => 'plants#wet'
-  get '/plants/medium' => 'plants#medium'
+  get '/plants/moderate' => 'plants#moderate'
   get '/plants/dry' => 'plants#dry'
   resources :plants
+
+  resources :events do
+    resources :exceptions, module: :events
+  end
 
 end
