1bf5924d275de0298e04a9a427c8e8467ec873ab
Jason Rowland <jasonrowland86>
2017-09-19 13:22:09 -0400

    Schema changes for events model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916025040) do
+ActiveRecord::Schema.define(version: 20170919134251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.text "recurring"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "plants", force: :cascade do |t|
     t.string "name"
