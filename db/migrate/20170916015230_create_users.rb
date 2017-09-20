e690cf2e3ae8d96b30f6239c4f2e34278c3d4297
Jason Rowland <jasonrowland86>
2017-09-20 11:47:21 -0400

    Added columns to users table for phone-number and name

--- db/migrate/20170916015230_create_users.rb
+++ db/migrate/20170916015230_create_users.rb
@@ -2,13 +2,15 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :username
+      t.string :name
+      t.string :email
+      t.string :phone_number
       t.string :session_token
       t.string :password_digest
 
-
       t.timestamps
     end
-    
+
     add_index :users, :username
     add_index :users, :session_token
   end
--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20170919134251) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
     t.string "session_token"
     t.string "password_digest"
     t.datetime "created_at", null: false
